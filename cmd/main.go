889d1591f3167d7c71c50eb4d777c51be54d98fd
Evgeniy Pereverzev <kamtorg@bk.ru>
2020-01-27 08:17:07 +0300

    Add app stub

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/eai-rts/asec"
+)
+
+func main()  {
+	fmt.PrintLn(asec.BEGINNING)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/eai-rts/asec
+
+go 1.13
+
+require github.com/uniplaces/carbon v0.1.6
